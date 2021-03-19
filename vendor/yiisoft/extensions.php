53c51defb74aa0c20ac83d96680e77b31a2fcbe0
rspsuresh <suresh.p@rapidcareitservices.com>
2021-03-19 22:00:49 +0530

    scf

---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,69 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap/src',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '2.1.16.0',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug/src',
+    ),
+  ),
+  'yiisoft/yii2-gii' => 
+  array (
+    'name' => 'yiisoft/yii2-gii',
+    'version' => '2.1.4.0',
+    'alias' => 
+    array (
+      '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii/src',
+    ),
+  ),
+  'mohorev/yii2-upload-behavior' => 
+  array (
+    'name' => 'mohorev/yii2-upload-behavior',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@mohorev/file' => $vendorDir . '/mohorev/yii2-upload-behavior/src',
+    ),
+  ),
+  'nikosid/yii2-cloudinary' => 
+  array (
+    'name' => 'nikosid/yii2-cloudinary',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@nikosid/cloudinary' => $vendorDir . '/nikosid/yii2-cloudinary/src',
+    ),
+  ),
+  'yiisoft/yii2-faker' => 
+  array (
+    'name' => 'yiisoft/yii2-faker',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/faker' => $vendorDir . '/yiisoft/yii2-faker/src',
+    ),
+  ),
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer/src',
+    ),
+  ),
+);
