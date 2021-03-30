9e565508aca8cb4ddc6115566aee62e2cb5dffe1
rspsuresh <suresh.p@rapidcareitservices.com>
2021-03-30 10:19:25 +0530

    forgt password

--- views/site/login.php
+++ views/site/login.php
@@ -43,27 +43,45 @@
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
-        <p class="login-box-msg">Sign in to start your session</p>
-        <form  id="loginform" name="loginform" method="post">
-            <div class="form-group has-feedback">
-                <input type="text" class="form-control" name="username"
-                       placeholder="Email" required>
-<!--                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>-->
-            </div>
-            <div class="form-group has-feedback">
-                <input type="password" class="form-control" name="password"
-                       placeholder="Password" required>
-<!--                <span class="glyphicon glyphicon-lock form-control-feedback"></span>-->
-            </div>
-            <div id="incorrect" class="error" style="display: none"><p class="text-center" >Incorrect Username and Password</p></div>
-            <div class="row">
-                <!-- /.col -->
-                <div class="col-xs-offset-8 col-xs-4">
-                    <button  class="btn btn-primary btn-block btn-flat">Sign In</button>
+        <div id="loginformdiv">
+            <b><p class="login-box-msg">Sign in to start your session</p></b>
+            <form  id="loginform" name="loginform" method="post">
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" name="username"
+                           placeholder="Email" required>
+                    <!--                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>-->
                 </div>
-                <!-- /.col -->
-            </div>
-        </form>
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-control" name="password"
+                           placeholder="Password" required>
+                    <!--                <span class="glyphicon glyphicon-lock form-control-feedback"></span>-->
+                </div>
+                <div id="incorrect" class="error" style="display: none"><p class="text-center" >Incorrect Username and Password</p></div>
+                <div class="row">
+                    <!-- /.col -->
+                    <div class="col-xs-offset-8 col-xs-4">
+                        <button  class="btn btn-primary btn-block btn-flat">Sign In</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+            <a style="cursor: pointer" onclick="forgotpasswordsection()">I forgot my password</a>
+        </div>
+        <div id="forgotpassworddiv" style="display:none">
+            <b> <p class="login-box-msg">Forgot password</p></b>
+            <form  id="forgotpassword" name="forgotpassword" method="post">
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" name="username"
+                           placeholder="Email" required>
+                </div>
+                <div class="row">
+                    <div class="col-xs-offset-8 col-xs-4">
+                        <button  class="btn btn-primary btn-block btn-flat">Submit</button>
+                    </div>
+                </div>
+            </form>
+            <a style="cursor: pointer" onclick="backtologin()">Back to Login</a>
+        </div>
     </div>
     <!-- /.login-box-body -->
 </div>
@@ -112,6 +130,14 @@
             return false;
         }
     });
+    function forgotpasswordsection(){
+        $("#loginformdiv").hide();
+        $("#forgotpassworddiv").show();
+    }
+    function backtologin(){
+        $("#loginformdiv").show();
+        $("#forgotpassworddiv").hide();
+    }
 </script>
 </body>
 </html>
